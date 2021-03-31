6951473da8e1c25a323d83ba7c48b2dc1f36be66
2021-DEV2-015 <andrei@nodalview.com>
2021-03-31 13:18:26 +0200

    Added 5 minute blocks class with base implementation

---
+++ Berlin Clock/ClockMultiMinute.swift
@@ -0,0 +1,26 @@
+//
+//  ClockMultiMinute.swift
+//  Berlin Clock
+//
+//  Created by 2021-DEV2-015 on 31/03/2021.
+//
+
+import Foundation
+import UIKit
+
+class ClockMultiMinute: ClockLampCountableProtocol {
+    
+    var date: Date
+    
+    required init(date: Date) {
+        self.date = date
+    }
+    
+    func numberOfLamps() -> Int {
+        return 0
+    }
+    
+    func colorForLamp(at index: Int) -> UIColor {
+        return .black
+    }
+}
