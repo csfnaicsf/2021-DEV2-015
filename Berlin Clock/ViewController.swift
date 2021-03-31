2e6b490e6a6a5981b005cc5ddbbe2600e824da20
2021-DEV2-015 <andrei@nodalview.com>
2021-03-31 11:39:36 +0200

    Project setup, initial commit

---
+++ Berlin Clock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Berlin Clock
+//
+//  Created by 2021-DEV2-015 on 31/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
