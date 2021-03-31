52f702f5a395be947c52be88cea852ea755bc9bd
2021-DEV2-015 <andrei@nodalview.com>
2021-03-31 13:46:20 +0200

    Added test cases for seconds class, tests fail

--- Berlin ClockTests/ClockSecondTests.swift
+++ Berlin ClockTests/ClockSecondTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import Berlin_Clock
 
 class ClockSecondTests: XCTestCase {
 
@@ -16,17 +17,21 @@ class ClockSecondTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testEvenSeconds() {
+        guard let evenSecond = Date(from: "00:00:42", format: "HH:mm:ss") else {
+            XCTFail("A valid date should be created from the given format")
+            return
         }
+        XCTAssertEqual(ClockSecond(date: evenSecond).colorForLamp(), .red, "Even seconds should have a red lamp")
+    }
+    
+    func testOddSeconds() {
+        guard let oddSecond = Date(from: "00:00:21", format: "HH:mm:ss") else {
+            XCTFail("A valid date should be created from the given format")
+            return
+        }
+        XCTAssertEqual(ClockSecond(date: oddSecond).colorForLamp(), .white, "Odd seconds should have a white lamp")
     }
 
 }
