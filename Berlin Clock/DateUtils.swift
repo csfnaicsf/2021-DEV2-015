09e8bbf5d7700c24f677808a95ed0fb2bc533e6f
2021-DEV2-015 <andrei@nodalview.com>
2021-03-31 11:57:09 +0200

    Added date components Added tests for year, month and day Updated existing test

--- Berlin Clock/DateUtils.swift
+++ Berlin Clock/DateUtils.swift
@@ -9,6 +9,10 @@ import Foundation
 
 extension Date {
     
+    enum DateComponentType {
+        case year, month, day, hour, minute, second, nanosecond
+    }
+    
     init?(from string: String, format: String) {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = format
@@ -16,6 +20,34 @@ extension Date {
             return nil
         }
         self.init(timeInterval: 0, since: date)
+    }
+    
+    func component(_ component: DateComponentType) -> Int? {
+        let components = Calendar.current.dateComponents([
+            Calendar.Component.year,
+            Calendar.Component.month,
+            Calendar.Component.day,
+            Calendar.Component.hour,
+            Calendar.Component.minute,
+            Calendar.Component.second,
+            Calendar.Component.nanosecond
+        ], from: self)
+        switch component {
+        case .year:
+            return components.year
+        case .month:
+            return components.month
+        case .day:
+            return components.day
+        case .hour:
+            return components.hour
+        case .minute:
+            return components.minute
+        case .second:
+            return components.second
+        case .nanosecond:
+            return components.nanosecond
+        }
     }
     
 }
--- Berlin ClockTests/DateUtilsTest.swift
+++ Berlin ClockTests/DateUtilsTest.swift
@@ -17,10 +17,68 @@ class DateUtilsTest: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
+    func testYear() throws {
+        let validDate = Date(from: "2021", format: "yyyy")
+        XCTAssertNotNil(validDate, "A valid date should be created from a date format specifying a valid year")
+        XCTAssertEqual(validDate!.component(.year), 2021, "The year of the date should be the same as the year in the string")
+        
+        let invalidDate = Date(from: "-2021", format: "yyyy")
+        XCTAssertNil(invalidDate, "A nil date should be created from a date format specifying an invalid year")
+    }
+    
+    func testMonth() throws {
+        let dateWithValidMonthLeadingZero = Date(from: "03", format: "MM")
+        XCTAssertNotNil(dateWithValidMonthLeadingZero, "A valid date should be created from a date format specifying a valid month with a leading zero")
+        XCTAssertEqual(dateWithValidMonthLeadingZero!.component(.month), 3, "The month of the date should be the same as the month in the string")
+        
+        let dateWithValidMonthNoLeadingZero = Date(from: "3", format: "M")
+        XCTAssertNotNil(dateWithValidMonthNoLeadingZero, "A valid date should be created from a date format specifying a valid month without a leading zero")
+        XCTAssertEqual(dateWithValidMonthNoLeadingZero!.component(.month), 3, "The month of the date should be the same as the month in the string")
+        
+        let dateWithInvalidMonthLeadingZero = Date(from: "00", format: "MM")
+        XCTAssertNil(dateWithInvalidMonthLeadingZero, "A nil date should be created from a date format specifying an invalid month with a leading zero")
+        
+        let dateWithInvalidMonthNoLeadingZero = Date(from: "0", format: "M")
+        XCTAssertNil(dateWithInvalidMonthNoLeadingZero, "A nil date should be created from a date format specifying an invalid month without a leading zero")
+        
+        let dateWithInvalidMonth = Date(from: "13", format: "MM")
+        XCTAssertNil(dateWithInvalidMonth, "A nil date should be created from a date format specifying an invalid month")
+    }
+    
+    func testDay() throws {
+        // Adding year and month as well, otherwise test will fail depending on the year & month when it is executed
+        let dateWithValidDayLeadingZero = Date(from: "2021-03-05", format: "yyyy-MM-dd")
+        XCTAssertNotNil(dateWithValidDayLeadingZero, "A valid date should be created from a date format specifying a valid day with a leading zero")
+        XCTAssertEqual(dateWithValidDayLeadingZero!.component(.year), 2021, "The year of the date should be the same as the year in the string")
+        XCTAssertEqual(dateWithValidDayLeadingZero!.component(.month), 3, "The month of the date should be the same as the month in the string")
+        XCTAssertEqual(dateWithValidDayLeadingZero!.component(.day), 5, "The day of the date should be the same as the day in the string")
+        
+        let dateWithValidDayNoLeadingZero = Date(from: "2021-03-5", format: "yyyy-MM-d")
+        XCTAssertNotNil(dateWithValidDayNoLeadingZero, "A valid date should be created from a date format specifying a valid day without a leading zero")
+        XCTAssertEqual(dateWithValidDayNoLeadingZero!.component(.year), 2021, "The year of the date should be the same as the year in the string")
+        XCTAssertEqual(dateWithValidDayNoLeadingZero!.component(.month), 3, "The month of the date should be the same as the month in the string")
+        XCTAssertEqual(dateWithValidDayNoLeadingZero!.component(.day), 5, "The day of the date should be the same as the day in the string")
+       
+        let dateWithInvalidDayLeadingZero = Date(from: "2021-03-00", format: "yyyy-MM-dd")
+        XCTAssertNil(dateWithInvalidDayLeadingZero, "A nil date should be created from a date format specifying an invalid day with a leading zero")
+        
+        let dateWithInvalidMonthNoLeadingZero = Date(from: "2021-03-0", format: "yyyy-MM-d")
+        XCTAssertNil(dateWithInvalidMonthNoLeadingZero, "A nil date should be created from a date format specifying an invalid day without a leading zero")
+        
+        let dateWithInvalidDay = Date(from: "2021-04-31", format: "yyyy-MM-dd")
+        XCTAssertNil(dateWithInvalidDay, "A nil date should be created from a date format specifying an invalid day")
+    }
+    
     func testDate() throws {
         let date = Date(from: "2021-03-31 09:41", format: "yyyy-MM-dd HH:mm")
         XCTAssertNotNil(date, "A valid date should be created from a date format specifying a valid date")
+        XCTAssertEqual(date!.component(.year), 2021, "The year of the date should be the same as the year in the string")
+        XCTAssertEqual(date!.component(.month), 3, "The month of the date should be the same as the month in the string")
+        XCTAssertEqual(date!.component(.day), 31, "The day of the date should be the same as the day in the string")
+        XCTAssertEqual(date!.component(.hour), 9, "The hour of the date should be the same as the hour in the string")
+        XCTAssertEqual(date!.component(.minute), 41, "The minute of the date should be the same as the minute in the string")
     }
+ 
 
 }
