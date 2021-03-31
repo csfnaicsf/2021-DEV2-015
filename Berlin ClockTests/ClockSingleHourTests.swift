3f69ae842fcac0e81a2c931456ea23772916b405
2021-DEV2-015 <andrei@nodalview.com>
2021-03-31 13:12:58 +0200

    Added tests cases for lamp color for 1 hour blocks, tests fail

--- Berlin ClockTests/ClockSingleHourTests.swift
+++ Berlin ClockTests/ClockSingleHourTests.swift
@@ -24,19 +24,31 @@ class ClockSingleHourTests: XCTestCase {
             XCTFail("A valid date should be created from the given format")
             return
         }
-        XCTAssertEqual(ClockSingleHour(date: lowerHourDate).numberOfLamps(), counter, "\(counter) lamps should be on at midnight")
+        var singleHourClock = ClockSingleHour(date: lowerHourDate)
+        XCTAssertEqual(singleHourClock.numberOfLamps(), counter, "\(counter) lamps should be on at 12AM")
+        for index in (counter + 1)...4 {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .white, "Lamp \(counter) should be white at 12AM")
+        }
         
         guard let midHourDate = Date(from: "10:00:00", format: "HH:mm:ss") else {
             XCTFail("A valid date should be created from the given format")
             return
         }
-        XCTAssertEqual(ClockSingleHour(date: midHourDate).numberOfLamps(), counter, "\(counter) lamps should be on at 10AM")
+        singleHourClock = ClockSingleHour(date: midHourDate)
+        XCTAssertEqual(singleHourClock.numberOfLamps(), counter, "\(counter) lamps should be on at 10AM")
+        for index in (counter + 1)...4 {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .white, "Lamp \(counter) should be white at 10AM")
+        }
         
         guard let upperHourDate = Date(from: "20:00:00", format: "HH:mm:ss") else {
             XCTFail("A valid date should be created from the given format")
             return
         }
-        XCTAssertEqual(ClockSingleHour(date: upperHourDate).numberOfLamps(), counter, "\(counter) lamps should be on at 8PM")
+        singleHourClock = ClockSingleHour(date: upperHourDate)
+        XCTAssertEqual(singleHourClock.numberOfLamps(), counter, "\(counter) lamps should be on at 8PM")
+        for index in (counter + 1)...4 {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .white, "Lamp \(counter) should be white at 8PM")
+        }
     }
     
     func testFirstLamp() throws {
@@ -45,19 +57,40 @@ class ClockSingleHourTests: XCTestCase {
             XCTFail("A valid date should be created from the given format")
             return
         }
-        XCTAssertEqual(ClockSingleHour(date: lowerHourDate).numberOfLamps(), counter, "\(counter) lamp should be on at 6AM")
+        var singleHourClock = ClockSingleHour(date: lowerHourDate)
+        XCTAssertEqual(singleHourClock.numberOfLamps(), counter, "\(counter) lamp should be on at 6AM")
+        for index in 1...counter {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .red, "Lamp \(counter) should be red 6AM")
+        }
+        for index in (counter + 1)...4 {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .white, "Lamp \(counter) should be white at 6AM")
+        }
         
         guard let midHourDate = Date(from: "11:00:00", format: "HH:mm:ss") else {
             XCTFail("A valid date should be created from the given format")
             return
         }
-        XCTAssertEqual(ClockSingleHour(date: midHourDate).numberOfLamps(), counter, "\(counter) lamp should be on at 11AM")
+        singleHourClock = ClockSingleHour(date: midHourDate)
+        XCTAssertEqual(singleHourClock.numberOfLamps(), counter, "\(counter) lamp should be on at 11AM")
+        for index in 1...counter {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .red, "Lamp \(counter) should be red at 11AM")
+        }
+        for index in (counter + 1)...4 {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .white, "Lamp \(counter) should be white at 11AM")
+        }
         
         guard let upperHourDate = Date(from: "21:00:00", format: "HH:mm:ss") else {
             XCTFail("A valid date should be created from the given format")
             return
         }
-        XCTAssertEqual(ClockSingleHour(date: upperHourDate).numberOfLamps(), counter, "1 lamp should be on at 9PM")
+        singleHourClock = ClockSingleHour(date: upperHourDate)
+        XCTAssertEqual(singleHourClock.numberOfLamps(), counter, "1 lamp should be on at 9PM")
+        for index in 1...counter {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .red, "Lamp \(counter) should be red at 9PM")
+        }
+        for index in (counter + 1)...4 {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .white, "Lamp \(counter) should be white at 9PM")
+        }
     }
     
     func testSecondLamp() throws {
@@ -66,19 +99,40 @@ class ClockSingleHourTests: XCTestCase {
             XCTFail("A valid date should be created from the given format")
             return
         }
-        XCTAssertEqual(ClockSingleHour(date: lowerHourDate).numberOfLamps(), counter, "\(counter) lamps should be on at 7AM")
+        var singleHourClock = ClockSingleHour(date: lowerHourDate)
+        XCTAssertEqual(singleHourClock.numberOfLamps(), counter, "\(counter) lamps should be on at 7AM")
+        for index in 1...counter {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .red, "Lamp \(counter) should be red at 7AM")
+        }
+        for index in (counter + 1)...4 {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .white, "Lamp \(counter) should be white at 7AM")
+        }
         
         guard let midHourDate = Date(from: "12:00:00", format: "HH:mm:ss") else {
             XCTFail("A valid date should be created from the given format")
             return
         }
-        XCTAssertEqual(ClockSingleHour(date: midHourDate).numberOfLamps(), counter, "\(counter) lamps should be on at 12PM")
+        singleHourClock = ClockSingleHour(date: midHourDate)
+        XCTAssertEqual(singleHourClock.numberOfLamps(), counter, "\(counter) lamps should be on at 12PM")
+        for index in 1...counter {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .red, "Lamp \(counter) should be red at 12PM")
+        }
+        for index in (counter + 1)...4 {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .white, "Lamp \(counter) should be white at 12PM")
+        }
         
         guard let upperHourDate = Date(from: "22:00:00", format: "HH:mm:ss") else {
             XCTFail("A valid date should be created from the given format")
             return
         }
-        XCTAssertEqual(ClockSingleHour(date: upperHourDate).numberOfLamps(), counter, "\(counter) lamps should be on at 10PM")
+        singleHourClock = ClockSingleHour(date: upperHourDate)
+        XCTAssertEqual(singleHourClock.numberOfLamps(), counter, "\(counter) lamps should be on at 10PM")
+        for index in 1...counter {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .red, "Lamp \(counter) should be red at 10PM")
+        }
+        for index in (counter + 1)...4 {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .white, "Lamp \(counter) should be white at 10PM")
+        }
     }
     
     func testThirdLamp() throws {
@@ -87,19 +141,40 @@ class ClockSingleHourTests: XCTestCase {
             XCTFail("A valid date should be created from the given format")
             return
         }
-        XCTAssertEqual(ClockSingleHour(date: lowerHourDate).numberOfLamps(), counter, "\(counter) lamps should be on at 8AM")
+        var singleHourClock = ClockSingleHour(date: lowerHourDate)
+        XCTAssertEqual(singleHourClock.numberOfLamps(), counter, "\(counter) lamps should be on at 8AM")
+        for index in 1...counter {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .red, "Lamp \(counter) should be red at 8AM")
+        }
+        for index in (counter + 1)...4 {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .white, "Lamp \(counter) should be white at 8AM")
+        }
         
         guard let midHourDate = Date(from: "13:00:00", format: "HH:mm:ss") else {
             XCTFail("A valid date should be created from the given format")
             return
         }
-        XCTAssertEqual(ClockSingleHour(date: midHourDate).numberOfLamps(), counter, "\(counter) lamps should be on at 1PM")
+        singleHourClock = ClockSingleHour(date: midHourDate)
+        XCTAssertEqual(singleHourClock.numberOfLamps(), counter, "\(counter) lamps should be on at 1PM")
+        for index in 1...counter {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .red, "Lamp \(counter) should be red at 1PM")
+        }
+        for index in (counter + 1)...4 {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .white, "Lamp \(counter) should be white at 1PM")
+        }
         
         guard let upperHourDate = Date(from: "23:00:00", format: "HH:mm:ss") else {
             XCTFail("A valid date should be created from the given format")
             return
         }
-        XCTAssertEqual(ClockSingleHour(date: upperHourDate).numberOfLamps(), counter, "\(counter) lamps should be on at 11PM")
+        singleHourClock = ClockSingleHour(date: upperHourDate)
+        XCTAssertEqual(singleHourClock.numberOfLamps(), counter, "\(counter) lamps should be on at 11PM")
+        for index in 1...counter {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .red, "Lamp \(counter) should be red at 11PM")
+        }
+        for index in (counter + 1)...4 {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .white, "Lamp \(counter) should be white at 11PM")
+        }
     }
     
     func testFourthLamp() throws {
@@ -108,19 +183,31 @@ class ClockSingleHourTests: XCTestCase {
             XCTFail("A valid date should be created from the given format")
             return
         }
+        var singleHourClock = ClockSingleHour(date: lowerHourDate)
         XCTAssertEqual(ClockSingleHour(date: lowerHourDate).numberOfLamps(), counter, "\(counter) lamps should be on at 9AM")
+        for index in 1...counter {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .red, "Lamp \(counter) should be red at 9AM")
+        }
         
         guard let midHourDate = Date(from: "14:00:00", format: "HH:mm:ss") else {
             XCTFail("A valid date should be created from the given format")
             return
         }
-        XCTAssertEqual(ClockSingleHour(date: midHourDate).numberOfLamps(), counter, "\(counter) lamps should be on at 2PM")
+        singleHourClock = ClockSingleHour(date: midHourDate)
+        XCTAssertEqual(singleHourClock.numberOfLamps(), counter, "\(counter) lamps should be on at 2PM")
+        for index in 1...counter {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .red, "Lamp \(counter) should be red at 2PM")
+        }
         
         guard let upperHourDate = Date(from: "19:00:00", format: "HH:mm:ss") else {
             XCTFail("A valid date should be created from the given format")
             return
         }
-        XCTAssertEqual(ClockSingleHour(date: upperHourDate).numberOfLamps(), counter, "\(counter) lamps should be on at 7PM")
+        singleHourClock = ClockSingleHour(date: upperHourDate)
+        XCTAssertEqual(singleHourClock.numberOfLamps(), counter, "\(counter) lamps should be on at 7PM")
+        for index in 1...counter {
+            XCTAssertEqual(singleHourClock.colorForLamp(at: index), .red, "Lamp \(counter) should be red at 7PM")
+        }
     }
 
 }
